3b98f88f6f54ef8ab0141411fa0b46018f8b94fe
Ramiz Ortiqov <ramizortiqov07@gmail.com>
2026-02-03 16:11:05 +0300

    Update BookCard.tsx

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -62,7 +62,6 @@ export const BookCard: React.FC<BookCardProps> = ({ book, onClick }) => {
         {/* Блок цены */}
         <div className="mt-3 pt-3 border-t border-gray-50 flex items-center justify-between">
           <div className="flex flex-col">
-            <span className="text-[10px] text-gray-400 font-medium">Цена:</span>
             <span className={`text-lg font-bold leading-none ${isAvailable ? 'text-blue-600' : 'text-gray-400'}`}>
               {formattedPrice} С
             </span>
